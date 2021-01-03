df4af07bf3ef0e44de643492d0153b904b229bf5
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-01-03 16:10:16 +0530

    Added stack impl.s for binary tree

--- binary-tree.cpp
+++ binary-tree.cpp
@@ -1,6 +1,6 @@
 /* Most amazing thing about this program:
 *  It only took one SEGFAULT, and that too
-*  was only due to me assigning pointers by: 
+*  was only due to assigning pointers by: 
 *       curr, prev = start;
 *  instead of:
 *       curr = prev = start;
@@ -14,6 +14,11 @@ b. pre-order:  first  process  the  root,  then  traverse  the  left  subtree,  
 c. post-order: first traverse the left subtree, then traverse the right subtree,then process the root
 */
 
+/*
+Input to build tree on pg. 139 (compile with SELFIN defined):
+1 2 4 -1 -1 5 6 -1 -1 -1 3 -1 7 -1 -1
+*/
+
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -21,7 +26,7 @@ using namespace std;
 struct node {
     struct node* left;
     struct node* right;
-    uint content;
+    int content;
 };
 
 void pre_order(struct node* n) {
@@ -54,39 +59,98 @@ void in_order(struct node* n) {
     in_order(n->right);
 }
 
-void efficient_pre_order(struct node* n) {
-    stack<struct node*> pointers;
-    pointers.push(n);
+void stack_pre_order(struct node* n) {
+    stack<struct node*> s;
+    s.push(n);
 
     struct node* curr;
-    while (!pointers.empty()) {
-        curr = pointers.top();
+    while (!s.empty()) {
+        curr = s.top();
         cout << curr->content << " ";
-        pointers.pop();
-
+        s.pop();
 
         if (curr->right != NULL) {
-            pointers.push(curr->right);
+            s.push(curr->right);
         }
         if (curr->left != NULL) {
-            pointers.push(curr->left);
+            s.push(curr->left);
         }
     }
 }
+
+// Recursively expands first left node then right node.
+// expand <- i.e. pushes children into stack (such that left child is on top)
+void post_order_expand(stack<struct node*>& s) {
+    struct node* curr = NULL;
+    while (curr != s.top()) { //i.e. terminates when both curr->right and curr->left are NULL
+        curr = s.top();
+
+        if (curr->right != NULL) {
+            s.push(curr->right);
+            post_order_expand(s);
+        }
+        if (curr->left != NULL) {
+            s.push(curr->left);
+            post_order_expand(s);
+        }
+    }
+}
+
+void stack_post_order(struct node* n) {
+    stack<struct node*> s;
+    s.push(n);
+
+    struct node* curr = s.top();
+    post_order_expand(s);
+    
+    while (!s.empty()) {
+        cout << s.top()->content << " ";
+        s.pop();
+    }
+}
+
+
+#ifdef SELFIN
+void build_bin_tree (struct node* n) {    
+    int in;
+
+    cout << "Enter value at left of " << n->content << ": ";
+    cin >> in;
+
+    if (in != -1) {
+        n->left = (struct node*) malloc(sizeof(struct node));
+        n->left->content = in;
+        build_bin_tree(n->left);
+    }
+
+    cout << "Enter value at right of " << n->content << ": ";
+    cin >> in;
+
+    if (in != -1) {
+        n->right = (struct node*) malloc(sizeof(struct node));
+        n->right->content = in;
+        build_bin_tree(n->right);
+    }
+}
+#endif
 
 int main() {
     struct node* start = (struct node*) malloc(sizeof(struct node));
 
     // init start with value
-    uint in;
-    cin >> in;
-    start->content = in;
+    #ifdef SELFIN
+    cout << "Enter initial value: ";
+    cout << "You can input -1 to finish adding a right/left node" << endl;
+    #endif
 
+    cin >> start->content;
 
     struct node *curr, *prev;
     struct node **fin;
 
-    //impl. unbalanced binary tree
+    //construct a binary tree
+    #ifndef SELFIN
+    int in;
     while (cin >> in) {
         curr = prev = start;
         while (curr != NULL) {
@@ -105,6 +169,11 @@ int main() {
         *fin = (struct node*) malloc(sizeof(struct node));
         (*fin)->content = in;
     }
+    #endif
+
+    #ifdef SELFIN
+    build_bin_tree(start);
+    #endif
 
     cout << "Post order: ";
     post_order(start);
@@ -118,7 +187,12 @@ int main() {
     in_order(start);
     cout << endl;
 
-    cout << "Efficient pre order: ";
-    efficient_pre_order(start);
+    cout << "Stack pre order: ";
+    stack_pre_order(start);
     cout << endl;
+
+    cout << "Stack post order: ";
+    stack_post_order(start);
+    cout << endl;
+
 }
