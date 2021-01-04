207cb4c0f86468eefe283dfd714b6315919b1f43
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-01-05 01:09:30 +0530

    DP sol

--- Calvins-Game-INOI1301/calvins-game.cpp
+++ Calvins-Game-INOI1301/calvins-game.cpp
@@ -3,6 +3,7 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+#define forn(name, start, end) for (size_t name = start; i < end; i++)
 
 #ifdef DEBUG
 void visualise_arr (int* arr, size_t n) {
@@ -13,74 +14,60 @@ void visualise_arr (int* arr, size_t n) {
 }
 #endif
 
-int dp[(size_t) 10e6 + 1];
-int b_dp[(size_t) 10e6 + 1];
-int arr[(size_t) 10e6 + 1];
+int A[(size_t) 10e6 + 1];
+int FW[(size_t) 10e6 + 1];
+int BW[(size_t) 10e6 + 1];
 
-uint n, k;
+uint N, K;
 
-
-// k = 2, n = 4
-// k = 2, n = 3 | k = 2, n = 2
-// k = 2, n = 2 | k = 2, n = 1
-int forward(int k, int n) { // k -> n
-    if (k > n)
+// computes highest score path from K to n
+int Forward (int n) {
+    if (n < K)
         return -INT_MAX;
-    else if (k == n)
-        return 0;
 
-    // if dp[n] has already been calculated
-    if (dp[n] != -INT_MAX)
-        return dp[n];
+    if (n == K)
+        return FW[n] = 0; // base case
     
-    #ifdef DEBUG
-    printf ("dp[%d] = max(forward(%d, %d) (%d), forward(%d, %d) (%d)) + arr[%d] (%d)\n", n, k, n - 1, forward(k, n - 1), k, n - 2, forward(k, n - 2), n, arr[n]);
-    #endif
-
-    return dp[n] = max(forward(k, n - 1), forward(k, n - 2)) + arr[n];
+    if (FW[n] != -INT_MAX)
+        return FW[n];
+    
+    FW[n] = A[n] + max(Forward(n - 1), Forward(n - 2));
+    return FW[n];
 }
 
-int backward(int end, int start) { // k -> n
-    if (end < start)
+int Backward (int n) {
+    if (n < 1)
         return -INT_MAX;
-    if (end == start) {
-        return 0;
-    }
 
-    // if dp[start] has already been calculated
-    if (b_dp[start] != -INT_MAX)
-        return b_dp[start];
+    if (n == 1)
+        return BW[1] = A[1]; // base case
     
-    #ifdef DEBUG
-    printf ("b_dp[%d] = max(%d, max(backward(%d, %d) (%d), backward(%d, %d) (%d))) + arr[%d] (%d)\n", start, b_dp[start], end, start + 1, backward(end, start + 1), end, start + 2, backward(end, start + 2), start, arr[start]);
-    #endif
-
-    return b_dp[start] = max(dp[start], max(backward(end, start + 1), backward(end, start + 2)) + arr[start]);
+    if (BW[n] != -INT_MAX)
+        return BW[n];
+    
+    BW[n] = A[n] + max(Backward(n - 1), Backward(n - 2));
+    return BW[n];
 }
 
 int main() {
-    cin >> n >> k;
-    k--;
+    cin >> N >> K;
+    K--;
 
-    for (size_t i = 0; i < n; i++) {
-        cin >> arr[i];
+    fill_n(FW, N, -INT_MAX);
+    fill_n(BW, N, -INT_MAX);
+
+    forn(i, 0, N) {
+        cin >> A[i];
     }
-    fill_n (dp, n, -INT_MAX);
-    fill_n (b_dp, n, -INT_MAX);
 
-    forward (k, n - 1);
+    Forward(N - 1);
+    Backward(N - 1);
 
-    dp[k] = arr[k];
+    // find with path with highest end value
+    int highest = -INT_MAX;
+    forn(i, K, N) {
+        highest = max(highest, FW[i] - A[i] + BW[i]);
+    }
 
-    #ifdef DEBUG
-    visualise_arr (dp, n);
-    #endif
-
-    backward (n - 1, 0);
-
-    #ifdef DEBUG
-    visualise_arr (b_dp, n);
-    #endif
-
-    cout << b_dp[0] << endl;
+    cout << highest << endl;
 }
