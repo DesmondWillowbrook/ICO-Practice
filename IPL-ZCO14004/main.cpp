950a222c71933161c25257e825ef2210f3b7e6ce
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-01-08 19:56:48 +0530

    IPL

--- IPL-ZCO14004/main.cpp
+++ IPL-ZCO14004/main.cpp
@@ -4,6 +4,32 @@ using namespace std;
 
 size_t N;
 
+#ifdef DEBUG
+void print_arr (int* arr, int n) {
+    for (size_t i = 0; i < N; i++) cout << arr[i] << " ";
+    cout << endl;
+}
+#endif
+
+int max_fee (int* arr, int sol[][2], int n, bool consecutive) {
+    if (n == 0) return arr[0];
+    if (n < 0) return -INT_MAX;
+
+    if (consecutive)
+        if (sol[n][0] != -INT_MAX) return max_fee (arr, sol, n - 1, false) + arr[n];
+        else return sol[n][0];
+    else
+        if (sol[n][1] != -INT_MAX) return max_fee (arr, sol, n - 1, false) + arr[n];
+        else return sol[n][1];
+}
+
 int main () {
-    
+    cin >> N;
+    int arr[N];
+    int sol[N][2]; // one index for if we select prev, the other for if we don't.
+    fill (&sol[0][0], &sol[0][0] + sizeof(sol), -INT_MAX);
+    for (size_t i = 0; i < N; i++) cin >> arr[i];
+
+    max_fee (arr, sol, N - 1, false);
+    cout << max (max_fee(arr, sol, N - 1, false), max_fee(arr, sol, N - 2, false));
 }
---
+++ IPL-ZCO14004/supw-ver.cpp
@@ -0,0 +1,45 @@
+
+// SUPW-modified version
+#include <bits/stdc++.h>
+
+using namespace std;
+
+size_t N;
+
+#ifdef DEBUG
+void print_arr (int* arr) {
+    for (size_t i = 0; i < N; i++) cout << arr[i] << " ";
+    cout << endl;
+}
+#endif
+
+int min_supw (int* arr, int* sol, int n) {
+    if (n < 0) return INT_MAX;
+    #ifndef DEBUG
+    else if (n < 3) return arr[n];
+    #endif
+    #ifdef DEBUG
+    else if (n < 3) {sol[n] = arr[n]; return arr[n];}
+    #endif
+    if (sol[n] != INT_MAX) return sol[n];
+    sol[n] = min(min_supw (arr, sol, n - 1), min(min_supw (arr, sol, n - 2), min_supw (arr, sol, n - 3))) + arr[n];
+    #ifdef DEBUG
+    print_arr(sol);
+    #endif
+    return sol[n];
+}
+
+int main () {
+    cin >> N;
+    int arr[N];
+    long long int sum = 0;
+    for (size_t i = 0; i < N; i++) {cin >> arr[i]; sum += arr[i];}
+    int sol[N];
+    fill_n (sol, N, INT_MAX);
+
+    #ifdef DEBUG // so sol can be visualised properly
+    sol[0] = arr[0];
+    #endif
+
+    cout << sum - min (min_supw(arr, sol, N - 1), min(min_supw(arr, sol, N - 2), min_supw(arr, sol, N - 3))) << endl;
+}
