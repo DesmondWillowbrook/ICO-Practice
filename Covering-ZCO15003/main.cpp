2d4d52271ed68b25dd506fb90ac21e97f0436f81
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-01-13 18:24:06 +0530

    covering

---
+++ Covering-ZCO15003/main.cpp
@@ -0,0 +1,52 @@
+/*
+Strategy:
+Sort ranges by start then end
+From first range, iterate until end of intersection and then increment answer
+
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+size_t N;
+
+#ifdef DEBUG
+void print_arr (pair<uint, uint>* arr, pair<uint, uint> bounds) {
+    printf ("bounds: (%d, %d)\n", bounds.first, bounds.second);
+    for (size_t i = 0; i < N; i++) {
+        printf ("(%d, %d)\n", arr[i].first, arr[i].second);
+    }
+}
+#endif
+
+int main () {
+    cin >> N;
+    pair<uint, uint> a[N];
+    for (size_t i = 0; i < N; i++) cin >> a[i].first >> a[i].second;
+
+    sort (a, a + N);
+
+    /*
+    By sorting, entries will be sorted start-wise, and within that, end-wise =>
+    (1, 3), (1, 4), (2, 1), ...
+    */
+
+    pair<uint, uint> bounds = a[0];
+    uint ans = 1;
+
+    for (size_t i = 0; i < N; i++) {
+        #ifdef DEBUG
+        print_arr (a, bounds);
+        #endif
+
+        // check if current element is out of range of bounds
+        // if so, then our intersection has ended.
+        if (bounds.second < a[i].first) {bounds = a[i]; ans++; continue;}
+
+        // if we get: (1, 3), (2, 4), then bounds should be: (2, 3)
+        bounds.first = max (bounds.first, a[i].first);
+        bounds.second = min (bounds.second, a[i].second);
+    }
+    cout << ans << endl;
+}
