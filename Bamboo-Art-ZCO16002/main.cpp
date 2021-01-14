ba9813fed38752329855d2a61cd13d176e9d2206
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-01-14 18:29:30 +0530

    Bamboo             zle

---
+++ Bamboo-Art-ZCO16002/main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+size_t N;
+
+int main () {
+    cin >> N;
+    uint a[N];
+    for (size_t i = 0; i < N; i++) cin >> a[i];
+    sort(a, a + N);
+
+}
