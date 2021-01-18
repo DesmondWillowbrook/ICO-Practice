7720fcf8c692926b0e66847f7a98cbe431f0bf66
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-01-19 00:55:34 +0530

    Fixed

--- Equal-Gifts-EQGIFTS/main.cpp
+++ Equal-Gifts-EQGIFTS/main.cpp
@@ -2,31 +2,24 @@
 
 using namespace std;
 
-size_t N;
-
-int min_diff (pair<int, int>* arr, int* sol, size_t n) {
-    if (sol[n] != INT_MAX) return sol[n];
-
-    #ifdef DEBUG
-    printf ("Min diff of pairs upto %ld: %d\n", n, min_diff (arr, sol, n - 1));
-    #endif
-
-    int curr_pair_diff = abs(arr[n].first - arr[n].second);
-    sol[n] = min (abs (min_diff (arr, sol, n - 1) + curr_pair_diff),
-                  abs (min_diff (arr, sol, n - 1) - curr_pair_diff));
-
-    return sol[n];
-}
-
 int main () {
-    cin >> N;
-    pair<int, int> arr[N];
-    for (size_t i = 0; i < N; i++) cin >> arr[i].first >> arr[i].second;
+    size_t N; cin >> N;
+    int arr[N], in1, in2;
 
-    int sol[N];
-    sol[1] = abs(arr[0].first - arr[0].second);
-    fill_n (sol + 2, N, INT_MAX);
+    for (size_t i = 0; i < N; i++) {
+        cin >> in1 >> in2;
+        arr[i] = abs (in1 - in2);
+    }
 
+    set<int> diffs ({0}), new_diffs;
+    for (size_t i = 0; i < N; i++) {
+        for (const int& diff: diffs) {
+            new_diffs.insert (abs(diff - arr[i]));
+            new_diffs.insert (abs(diff + arr[i]));
+        }
+        swap(diffs, new_diffs);
+        new_diffs.clear();
+    }
 
-    cout << min_diff(arr, sol, N - 1) << endl;
+    cout << *diffs.begin() << endl;
 }
