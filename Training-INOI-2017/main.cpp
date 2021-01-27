8ff20124a98c1890449a6551c99eb7f493129e95
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-01-27 13:14:13 +0530

    Approach 2: Correct one

--- Training-INOI-2017/main.cpp
+++ Training-INOI-2017/main.cpp
@@ -2,8 +2,6 @@
 
 using namespace std;
 
-uint max_str = 0, max_xp = 0;
-
 uint train (uint strength) {
     //find sum of digits
     uint digit_sum = 0;
@@ -12,64 +10,24 @@ uint train (uint strength) {
     return pow(digit_sum, 3);
 }
 
+uint maxXP (size_t n, size_t s) {
+
+}
+
 int main () {
     /*
-    Sol 1:
-    Maintain vec. of possible (XV, STR) combinations upto current city
-    Then delete pairs where XV < XV(max) && STR < STR(max),
-        where XV(max) and STR(max) represent the highest XV and STR values in all pairs.
-    Repeat until final city not reached.
-    Then print pair with highest XV
+    Sol 2:
+    Assertion: MaxXP upto N cities when training in S cities is constant
 
-    For pairs: .first == STR, .second == XP
+    MaxXP (n, s) = max (MaxXP (n - 1, s - 1), NaxXP (n - 1, s) + STR[s] * XP[n])
+    This is ok as training in any S cities will yield the same strength.
+    So there is a unique answer for each MaxXP (n, s), which is dependant on previous answers.
+    This is DP problem, then.
     */
 
     size_t N; cin >> N;
     
-    uint exp[N], init_str;
-    cin >> init_str;
-
-    for (size_t i = 0; i < N; i++) cin >> exp[i];
-
-    vector<pair<uint, uint>> v = {{init_str, 0}};
-    v.reserve(5000);
-
-    for (size_t i = 0; i < N; i++) {
-        // find max_str, max_xp
-        for (size_t j = 0; j < v.size(); j++) {
-            max_str = max(v[j].first, max_str);
-            max_xp = max(v[j].second, max_xp);
-        }
-
-        // removes all pairs where str < max_str AND xp < max_xp
-        remove_if(v.begin(), v.end(), [](pair<uint, uint> n) {return ((n.first < max_str) && (n.second < max_xp));});
-
-        // For each (XP, STR), add two pairs (and remove original): (XP + Str*XP[i], STR), (XP, train(STR))
-        // better yet, we will modify original and add one new (to be on the safe side)
-
-        #ifdef DEBUG
-        printf ("City %ld\n", i);
-        #endif
-
-        size_t vec_size = v.size();
-        for (size_t j = 0; j < vec_size; j++) {
-            #ifdef DEBUG
-            printf ("Considering STR: %d, XP: %d\n", v[j].first, v[j].second);
-            #endif
-
-            // case 1: Pikachu is trained
-            v.push_back ({v[j].first + train(v[j].first), v[j].second});
-
-            // case 2: Pikachu has battle (STR stays same, EXP += STR * XP[i])
-            v[j].second = v[j].second + (v[j].first * exp[i]);
-        }
-    }
-
-    for (size_t i = 0; i < v.size(); i++) {
-        #ifdef DEBUG
-        printf ("Considering STR: %d, XP: %d\n", v[i].first, v[i].second);
-        #endif
-        max_xp = max (max_xp, v[i].second);
-    }
-    cout << max_xp << endl;
+    uint exp[N], str[N];
+    cin >> str[0];
+    for (size_t i = 1; i < N; i++) str[i] = str[i - 1] + train(str[i - 1]);
 }
