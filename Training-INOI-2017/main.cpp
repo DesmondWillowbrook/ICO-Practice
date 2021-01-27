d3c0f8b8b349a794eadb2e8220944d8d86167752
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-01-27 21:58:31 +0530

    Sort of working

--- Training-INOI-2017/main.cpp
+++ Training-INOI-2017/main.cpp
@@ -10,8 +10,20 @@ uint train (uint strength) {
     return pow(digit_sum, 3);
 }
 
-uint maxXP (size_t n, size_t s) {
+int dp [5000][5001], str[5001], xp[5000];
 
+int maxXP (int n, int s) {
+    if (s > n || s < 0) return -INT_MAX;
+    if (dp[n][s] != -INT_MAX) return dp[n][s];
+
+    dp[n][s] =
+        max (/*train pokemon*/ maxXP (n - 1, s - 1), /*BATTLE*/ maxXP (n - 1, s) + (xp[n] * str[s]));
+
+    #ifdef DEBUG
+    printf ("Calculating: (%d cities, trained %d times, i.e. str = %d) = %d\n", n, s, str[s], dp[n][s]);
+    #endif
+
+    return dp [n][s];
 }
 
 int main () {
@@ -19,7 +31,8 @@ int main () {
     Sol 2:
     Assertion: MaxXP upto N cities when training in S cities is constant
 
-    MaxXP (n, s) = max (MaxXP (n - 1, s - 1), NaxXP (n - 1, s) + STR[s] * XP[n])
+    MaxXP (n, s) = max (MaxXP (n - 1, s - 1), MaxXP (n - 1, s) + STR[s] * XP[n]) if n > s
+                   MaxXP (n - 1, s - 1) if n == s
     This is ok as training in any S cities will yield the same strength.
     So there is a unique answer for each MaxXP (n, s), which is dependant on previous answers.
     This is DP problem, then.
@@ -27,7 +40,16 @@ int main () {
 
     size_t N; cin >> N;
     
-    uint exp[N], str[N];
     cin >> str[0];
-    for (size_t i = 1; i < N; i++) str[i] = str[i - 1] + train(str[i - 1]);
+    for (size_t i = 1; i <= N; i++) str[i] = str[i - 1] + train(str[i - 1]);
+    #ifdef DEBUG
+    for (size_t i = 0; i <= N; i++) printf ("Str. after training in %ld cities: %d\n", i, str[i]);
+    #endif
+
+    for (size_t i = 1; i <= N; i++) cin >> xp[i];
+    for (size_t i = 1; i <= N; i++) for (size_t j = 0; j <= N; j++) dp[i][j] = -INT_MAX;
+
+    int ans = 0;
+    for (size_t i = 0; i <= N; i++) ans = max (ans, maxXP(N, i));
+    cout << ans << endl;
 }
