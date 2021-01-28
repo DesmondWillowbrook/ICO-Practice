1b1d7b4bd3d8feb6d257f53292490ae9c80aff03
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-01-28 19:15:50 +0530

    Sorta works

--- Interesting-Sequences-INOI1902/main.cpp
+++ Interesting-Sequences-INOI1902/main.cpp
@@ -6,8 +6,23 @@ using namespace std;
 
 pair<uint, uint> a[3001];
 
-uint max_cost(uint* a, uint n) {
+// Checks if subseq. of length L is possible
+// with given cost.
+bool assert_lsc (size_t n, uint l, uint cost) {
+    uint dp[n]; //DP[i] defined as max. length of subsequence when it ends at index i.
+    dp[0] = 1;
 
+
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < i; j++) {
+            if (abs ( (int) a[i].second - (int) a[j].second) >= cost) {
+                dp[i] = max(dp[i], 1 + dp[j]);
+                if (dp[i] >= l) return true;
+            }
+        }
+    }
+
+    return false;
 }
 
 int main () {
@@ -34,7 +49,20 @@ int main () {
             We can also see that this function is Monotonic, i.e. after False for Cost A,
             it WILL give False for Cost A + 1, and so on...
 
-            So we can use Binary search!
-        */ 
+            So we can use Binary search to find the highest Cost that is possible!
+
+            Handling non-equality cases:
+        */
+
+        uint lo = 0, hi = l, mid;
+        while (lo < hi) {
+            mid = lo + ((hi - lo) / 2);
+            if (assert_lsc(n, l, mid))
+                lo = mid + 1;
+            else
+                hi = mid - 1;
+        }
+
+        cout << lo << endl;
     }
 }
