84c2494ad2151bf09488c0782878139fe46884ba
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-01-31 01:07:17 +0530

    Almost AC

--- Triathlon-INOI1201/main.cpp
+++ Triathlon-INOI1201/main.cpp
@@ -5,15 +5,22 @@ using namespace std;
 int main () {
     size_t N;
     cin >> N;
-    
-    size_t cobol = 1; long int end_task_time = 0;
-    for (size_t i = 0, a, b, c; i < N; i++) {
-        cin >> a >> b >> c;
-        cobol += a;
 
-        end_task_time += b + c;
-        end_task_time -= a;
+    // we store time for the two events
+    // in the first, so we don't need to write cmp function
+    pair<uint, uint> a[N];
+    for (size_t i = 0; i < N; i++) {
+        uint temp;
+        cin >> a[i].second >> a[i].first >> temp;
+        a[i].first += temp;
     }
 
-    cout << cobol + end_task_time << endl;
+    sort (a, a + N, greater<pair<uint, uint>>());
+    uint cobol_time = 0, end_task_trail_time = 0;
+    for (size_t i = 0; i < N; i++) {
+        cobol_time += a[i].second;
+        end_task_trail_time = max (cobol_time, cobol_time + a[i].first);
+    }
+
+    cout << end_task_trail_time << endl;
 }
