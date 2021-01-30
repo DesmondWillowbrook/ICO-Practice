03f6c8495900b1b637d5dbd9864ca298719f6d87
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-01-30 14:44:41 +0530

    Creation

---
+++ Table-Sum-INOI1202/main.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+size_t N;
+
+int main () {
+    cin >> N;
+    uint a [N];
+
+    uint max_ele = 0;
+    for (size_t i = 0; i < N; i++) {
+        cin >> a[i];
+    }
+
+    uint max_ele = 0;
+    for (size_t i = 0; i < N; i++) {
+        
+    }
+}
