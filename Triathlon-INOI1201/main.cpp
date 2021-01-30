1a0abadbfe6b72630706d5577822f246be0c53e9
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-01-30 16:15:25 +0530

    maybe works (doesn't)

--- Triathlon-INOI1201/main.cpp
+++ Triathlon-INOI1201/main.cpp
@@ -2,25 +2,17 @@
 
 using namespace std;
 
-uint comp (pair<uint, uint> a, pair<uint, uint> b) {
-    /*
-    Finds minimum overflow time and returns it, i.e. how much time will these two events in total take to finish.
-    */
-    return (a.first + a.second) < (b.first + b.second) ?
-        (a.first + a.second) - b.second :
-        (b.first + b.second) - a.first;
-}
-
 int main () {
     size_t N;
     cin >> N;
     
-    size_t cobol = 0, end_task_time = 0;
+    size_t cobol = 1; long int end_task_time = 0;
     for (size_t i = 0, a, b, c; i < N; i++) {
         cin >> a >> b >> c;
         cobol += a;
 
-        end_task_time = max (cobol + b + c, end_task_time);
+        end_task_time += b + c;
+        end_task_time -= a;
     }
 
     cout << cobol + end_task_time << endl;
