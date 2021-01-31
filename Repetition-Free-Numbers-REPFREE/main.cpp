c336e46406ec8247819c6e5c4e7f855b9ebd2eba
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-01-31 18:40:21 +0530

    Stylistic rewrite (tried to eliminate bugs)

--- Repetition-Free-Numbers-REPFREE/main.cpp
+++ Repetition-Free-Numbers-REPFREE/main.cpp
@@ -4,11 +4,15 @@ using namespace std;
 
 bool is_valid (long unsigned int num) {
     bool digits[9] = {0};
-    for (; num != 0; num /= 10) {
+
+    do {
         if (num % 10 == 0) return false;
+
         if (digits[(num % 10) - 1] == true) return false;
         else digits[(num % 10) - 1] = true;
-    }
+
+        num /= 10;
+    } while (num != 0);
 
     return true;
 }
