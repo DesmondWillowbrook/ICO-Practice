8805200c0f1890138e4dae4363d2029beab4ad0b
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-02-06 10:24:20 +0530

    try Floyd-Warshall

--- Free-Ticket-INOI1402/main.cpp
+++ Free-Ticket-INOI1402/main.cpp
@@ -2,46 +2,76 @@
 
 using namespace std;
 
+uint dist[230][230];
+
+#ifdef DEBUG
+int via[230][230];
+
+void print_path (uint i, uint j) {
+    if (i == j) return;
+
+    if (via[i][j] == -1) { // i.e. if they are neighbours
+        printf ("%d -> %d, ", i + 1, j + 1);
+    } else {
+        print_path (i, via[i][j]);
+        print_path (via[i][j], j);
+    }
+}
+#endif
+
 int main () {
     size_t cities, edges;
     cin >> cities >> edges;
 
-    vector<pair<uint, uint>> adj_list[cities];
+    for (size_t i = 0; i < cities; i++)
+        for (size_t j = 0; j < cities; j++)
+            if (i != j) dist[i][j] = UINT_MAX;
+            else dist[i][j] = 0;
+
     for (size_t i = 0; i < edges; i++) {
         uint a, b, weight;
         cin >> a >> b >> weight; a--; b--; // move to zero-based indexing
-        adj_list[a].push_back({weight, b});
-        adj_list[b].push_back({weight, a});
+
+        dist[a][b] = weight; dist[b][a] = weight;
+        #ifdef DEBUG
+        via[a][b] = -1; via[b][a] = -1;
+        #endif
     }
 
-    uint ans = 0;
-    uint distance[cities]; //bool visited[cities];
-    priority_queue<pair<uint, uint>> unvisited;
+    // use Floyd-Warshall
+    for (size_t i = 0; i < cities; i++)
+        for (size_t j = 0; j < cities; j++) {
+            for (size_t k = 0; k < cities; k++) {
+                if (dist[i][k] == UINT_MAX || dist[k][j] == UINT_MAX) continue;
+                dist[i][j] = min (dist[i][j], dist[i][k] + dist[k][j]);
 
-    /* run Djikstra on every node to find cheapest
-       max. dist. from all node pairs
-       We could've used Floyd-Warshall as well.
-    */
-    for (size_t src = 0; src < cities; src++) {
-        unvisited.push({0, src});
-        fill_n(distance, cities, INT_MAX);
-        distance[src] = 0;
-        //fill_n (visited, cities, false);
-
-        while (!unvisited.empty()) {
-            int node = unvisited.top().second; unvisited.pop(); //visited[node] = true;
-            int dist = distance[node];
-
-            for (auto ele: adj_list[node]) {
-                if (distance[ele.second] > dist + ele.first) {
-                    distance[ele.second] = dist + ele.first;
-                    unvisited.push({distance[ele.second], ele.second});
+                #ifdef DEBUG
+                if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                    via[i][j] = k; via[j][i] = k;
                 }
+                #endif
             }
         }
 
-        for (auto ele: distance) ans = max(ans, ele);
-    }
+    #ifdef DEBUG
+    pair<uint, uint> final_path;
+    #endif
+
+    uint ans = 0;
+    for (size_t i = 0; i < cities; i++)
+        for (size_t j = 0; j < cities; j++) {
+            if (ans < dist[i][j]) {
+                ans = dist[i][j];
+                #ifdef DEBUG
+                final_path.first = i; final_path.second = j;
+                #endif
+            }
+        }
+
+    #ifdef DEBUG
+    print_path (final_path.first, final_path.second);
+    cout << "\n";
+    #endif
 
     cout << ans << endl;
 }
