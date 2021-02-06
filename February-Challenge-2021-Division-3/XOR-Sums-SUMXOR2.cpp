49c945165d21cafe9f2a61e89813a1da583a9b18
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-02-06 11:44:17 +0530

    Create XOR sums

---
+++ February-Challenge-2021-Division-3/XOR-Sums-SUMXOR2.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main () {
+    size_t N, Q;
+    cin >> N;
+    long long int num[N], dp[N];
+
+    for (size_t i = 0; i < N; i++) {
+        cin >> num[i]; dp[i] = -1;
+    }
+
+    size_t q, ans;
+    while (Q--) {
+        cin >> q;
+        ans = 0;
+        for (size_t i = 0; i < q; i++) {
+
+        }
+    }
+}
