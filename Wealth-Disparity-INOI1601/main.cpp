a3e6386cce4235a58685b8fed08465a3e21bb6ea
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-02-07 11:28:25 +0530

    Fixed with memoization

--- Wealth-Disparity-INOI1601/main.cpp
+++ Wealth-Disparity-INOI1601/main.cpp
@@ -2,28 +2,35 @@
 
 using namespace std;
 
-long int ans = -INT_MAX;
+long int ans = -INT_MAX,
+w[(unsigned long) 10e5],
+p[(unsigned long) 10e5],
+max_ans[(unsigned long) 10e5];
 
-int main () {
-    size_t N; cin >> N;
-    long int w[N], p[N];
+size_t N;
+
+long int maxAnscestor (long int id) {
+    id--; // shift to zero-based
+
+    if (max_ans[id] != -INT_MAX) return max_ans[id];
+    if (p[id] == -1) return max_ans[id] = w[id];
+
+    max_ans[id] = max (w[id], maxAnscestor(p[id]));
+
+    #ifdef DEBUG
+    printf ("Max ancestor of %ld is %ld\n", id + 1, max_ans[id]);
+    #endif
+
+    return max_ans[id];
+}
+
+int main () { cin >> N;
+    fill_n (max_ans, N, -INT_MAX);
+
     for (size_t i = 0; i < N; i++) cin >> w[i];
     for (size_t i = 0; i < N; i++) cin >> p[i];
 
-    long int wealth, manager;
-    for (size_t i = 0; i < N; i++) {
-        wealth = w[i];        
-        for (manager = p[i];; manager = p[manager]) {
-            #ifdef DEBUG
-            printf ("Start: %ld, at: %ld\n", i, manager);
-            #endif
-
-            if (manager == -1) break;
-            else manager--;
-
-            ans = max (ans, w[manager] - wealth);
-        }
-    }
+    for (size_t i = 1; i <= N; i++) ans = max (ans, maxAnscestor(i) - w[i - 1]);
 
     cout << ans << endl;
 }
