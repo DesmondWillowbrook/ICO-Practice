800b8d7bc8c0b5883f620ba67cfdb9be03fac860
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-02-07 12:13:37 +0530

    Brackets

---
+++ Brackets-INOI1602/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+size_t N, K;
+
+int main () {
+    cin >> N >> K;
+    long int V[N]; uint B[N];
+    for (size_t i = 0; i < N; i++) cin >> V[i];
+    for (size_t i = 0; i < N; i++) cin >> B[i];
+
+    
+}
