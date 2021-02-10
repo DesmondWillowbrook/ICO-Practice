c66f16564730aa1dc284c0f925111ed3d5aa373f
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-02-10 20:39:42 +0530

    Solved Zero-One Tiles

---
+++ Zero-One-Tiles-TILES01/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+    // DP: f(N) = f(N - 1) + f(N - 2)
+    // base case: f(1) = 1, f(2) = 2
+    // i.e. Fibonacci series!!! :) :o
+
+    size_t N, curr = 1, prev = 0; cin >> N;
+    
+    for (size_t i = 0; i < N; i++) {
+        #ifdef DEBUG
+        cout << prev << " ";
+        #endif
+        size_t tmp_curr = curr;
+        curr += prev;
+        prev = tmp_curr;
+    }
+    #ifdef DEBUG
+    cout << endl;
+    #endif
+
+    cout << curr << endl;
+}
