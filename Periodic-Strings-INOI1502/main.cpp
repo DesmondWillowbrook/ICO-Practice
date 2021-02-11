7bc372306f22d76943d53a412664d1fa1a265940
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-02-11 22:14:47 +0530

    Attempt 1

--- Periodic-Strings-INOI1502/main.cpp
+++ Periodic-Strings-INOI1502/main.cpp
@@ -3,8 +3,36 @@
 using namespace std;
 
 size_t N, M;
+uint dp[150000];
+
+// modular exponentiation, req. to keep under limit
+uint mod_exp (uint a, uint b) {
+    uint ans = 1;
+    for (uint i = 0; i < b; i++) {
+        ans = (ans % M) * (a % M);
+    }
+    return ans;
+}
+
+uint pStr (uint n) {
+    if (dp[n - 1] != UINT_MAX) return dp[n - 1];
+
+    uint ans = mod_exp (2, n);
+    for (uint i = 1; i <= n/2; i++)
+        if (n % i == 0) ans = (ans % M) - (pStr(i) % M);
+        
+    ans %= M;
+
+    #ifdef DEBUG
+    printf ("Evaluating pStr(%d): %d\n", n, ans);
+    #endif
+
+    dp[n - 1] = ans;
+    return dp[n - 1];
+}
 
 int main () {
     cin >> N >> M;
-    
+    fill_n (dp, N, UINT_MAX);
+    cout << pStr(N) << endl;
 }
