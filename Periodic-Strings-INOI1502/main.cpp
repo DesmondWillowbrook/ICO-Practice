dc7ad8f8f713f29e9dda8368464b223d3921e36a
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-02-11 22:45:22 +0530

    Fix: Change to INT Why? Idk. This shouldn't be needed, yet WA without it.

--- Periodic-Strings-INOI1502/main.cpp
+++ Periodic-Strings-INOI1502/main.cpp
@@ -6,15 +6,15 @@ using namespace std;
 
 int main () {
     size_t N, M; cin >> N >> M;
-    uint dp[N + 1];
+    int dp[N + 1];
 
     dp[1] = 2;
     for (size_t i = 2; i <= N; i++)
-        dp[i] = (2 * dp[i - 1]) % M;
+        dp[i] = 2 * dp[i - 1] % M;
 
-    for (size_t i = 1; i <= N/2; i++)
-        for (size_t j = 2*i; j <= N; j += i /*iterates over all multiples of i*/)
-            dp[j] = (dp[j] - dp[i] + M /*Adding M ensures no neg. values*/) % M;
+    for (size_t i = 1; i < N; i++)
+        for (size_t j = 2*i; j <= N; j += i) /*iterates over all multiples of i*/
+            dp[j] = (dp[j] - dp[i] + M) % M; /*Adding M ensures no neg. values*/
 
     cout << dp[N] << endl;
 }
