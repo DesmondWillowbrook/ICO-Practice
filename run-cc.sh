bc4566c9eb1d10da34748270359e63e49cc15c56
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-02-13 14:40:55 +0530

    Modified run-cc for prettier printing

--- run-cc.sh
+++ run-cc.sh
@@ -1,2 +1,2 @@
 # Bash script to run CodeChef programs (folders where there is in.txt and main.cpp)
-g++ main.cpp -Wall --define DEBUG; cat in.txt; printf n; time ./a.out < in.txt
+g++ main.cpp -Wall --define DEBUG; cat in.txt; printf "\n\n"; time ./a.out < in.txt
