bfba54cc7b9b88bcbd77731e62044fc9ead96ead
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-02-15 14:42:48 +0530

    Fix

--- IPL-ZCO14004/main.cpp
+++ IPL-ZCO14004/main.cpp
@@ -5,8 +5,18 @@ using namespace std;
 size_t N;
 
 #ifdef DEBUG
-void print_arr (int* arr, int n) {
-    for (size_t i = 0; i < N; i++) cout << arr[i] << " ";
+void print_arr (int sol[][2]) {
+    for (size_t i = 0; i < N; i++) {
+        cout << "{ ";
+        if (sol[i][0] != INT_MAX)
+            cout << sol[i][0] << " ";
+        else cout << "- ";
+
+        if (sol[i][1] != INT_MAX)
+            cout << sol[i][1] << " ";
+        else cout << "- ";
+        cout << "} ";
+    }
     cout << endl;
 }
 #endif
@@ -18,11 +28,12 @@ int max_fee (int n, int* arr, int sol[][2], int consecutive) {
     if (sol[n][consecutive] != INT_MAX) return sol[n][consecutive];
 
     #ifdef DEBUG
-    printf ("Evaluating %d th when consec = %d\n", n, consecutive);
+    printf ("Curr: %d\n", arr[n]);
+    print_arr (sol);
     #endif
 
     if (consecutive == 0)
-        return sol[n][consecutive] = max (max_fee (n - 2, arr, sol, 0), max_fee (n - 2, arr, sol, 1)) + arr[n];
+        return sol[n][consecutive] = max (max (max_fee (n - 2, arr, sol, 0), max_fee (n - 2, arr, sol, 1)), max_fee (n - 3, arr, sol, 1)) + arr[n];
     else
         return sol[n][consecutive] = max_fee (n - 1, arr, sol, 0) + arr[n];
 }
@@ -36,7 +47,5 @@ int main () {
         sol[i][0] = INT_MAX; sol[i][1] = INT_MAX;
     }
 
-    cout << max(max(max(max_fee (N - 1, arr, sol, 1), max_fee (N - 1, arr, sol, 0)),
-                max(max_fee (N - 2, arr, sol, 1), max_fee (N - 2, arr, sol, 0))),
-                max(max_fee (N - 3, arr, sol, 1), max_fee (N - 3, arr, sol, 0))) << endl;
+    cout << max (max_fee (N - 2, arr, sol, 1), max(max_fee (N - 1, arr, sol, 0), max_fee (N - 1, arr, sol, 1))) << endl;
 }
