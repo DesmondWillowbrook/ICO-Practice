e1a2b1086658719868977929dce06efd3e6de1ee
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-02-17 00:33:18 +0530

    Attempt

--- Special-Sums-INOI1501/main.cpp
+++ Special-Sums-INOI1501/main.cpp
@@ -29,28 +29,39 @@ int main () {
 	long long int b_sum_till[N] = {b[0]}; // fill first element of b_sum_till with first element of b
 	for (size_t i = 1; i < N; i++) b_sum_till[i] = b_sum_till[i - 1] + b[i];
 
-	// set up best I till index, best I till index (in reverse, required for Case 2 (since i MUST be greater than j))
-	long long int best_i_fw[N + 1], best_i_bw[N + 1], ans = 0;
-	best_i_fw[0] = a[0] - b_sum_till[0]; best_i_bw[N - 1] = a[N - 1] + b_sum_till[N - 1];
+	// set up best I till index, best I till index in reverse (required for Case 2 (since i MUST be greater than j))
+	long long int best_i_fw[N], best_i_bw[N], ans = LONG_LONG_MIN;
+	best_i_fw[0] = a[0] - b_sum_till[0]; best_i_bw[N - 1] = a[N - 1] - b_sum_till[N - 1];
 	
 	for (size_t i = 1; i < N; i++) best_i_fw[i] = max(best_i_fw[i - 1], a[i] - b_sum_till[i]);
-	for (long int i = N - 2; i >= 0; i++) best_i_bw[i] = max (best_i_bw[i + 1], a[i] - b_sum_till[i]);
+	for (long int i = N - 2; i >= 0; i--) best_i_bw[i] = max (best_i_bw[i + 1], a[i] - b_sum_till[i]);
+
+	#ifdef DEBUG
+	for (size_t i = 0; i < N; i++) printf ("%ld\t", i);
+	cout << endl;
+	for (size_t i = 0; i < N; i++) printf ("%lld\t", b_sum_till[i]);
+	cout << endl;
+	for (size_t i = 0; i < N; i++) printf ("%lld\t", best_i_fw[i]);
+	cout << endl;
+	for (size_t i = 0; i < N; i++) printf ("%lld\t", best_i_bw[i]);
+	cout << endl;
+	#endif
 
 	for (size_t j = 1; j < N; j++) {
 		// Case 1
-		ans = max (ans, a[j] + b_sum_till[j - 1] + best_i_fw[j - 1]);
+		if (j > 0) ans = max (ans, a[j] + b_sum_till[j - 1] + best_i_fw[j - 1]);
 
 		#ifdef DEBUG
-		printf ("j = %ld: Case 1: %ld (a[%ld]) + %lld (b_sum_till[%ld] + %lld (best_i_fw[%ld])\n",
-			j, a[j], j, b_sum_till[j - 1], j - 1, best_i_fw[j - 1], j - 1);
+		if (j > 0) printf ("j = %ld: Case 1: %ld (a[%ld]) + %lld (b_sum_till[%ld] + %lld (best_i_fw[%ld]) = %lld\n",
+			j, a[j], j, b_sum_till[j - 1], j - 1, best_i_fw[j - 1], j - 1, a[j] + b_sum_till[j - 1] + best_i_fw[j - 1]);
 		#endif
 
 		// Case 2
-		ans = max (ans, a[j] + b_sum_till[j - 1] + b_sum_till[N - 1] + best_i_bw[j -1]);
+		if (j < N - 1) ans = max (ans, a[j] + b_sum_till[j - 1] + b_sum_till[N - 1] + best_i_bw[j + 1]);
 
 		#ifdef DEBUG
-		printf ("j = %ld: Case 2: %ld (a[%ld]) + %lld (b_sum_till[%ld] + %lld (b_sum_till[%ld]) + %lld (best_i_fw[%ld])\n",
-			j, a[j], j, b_sum_till[j - 1], j - 1, b_sum_till[N - 1], N - 1, best_i_fw[j - 1], j - 1);
+		 if (j < N - 1) printf ("j = %ld: Case 2: %ld (a[%ld]) + %lld (b_sum_till[%ld] + %lld (b_sum_till[%ld]) + %lld (best_i_bw[%ld]) = %lld\n",
+			j, a[j], j, b_sum_till[j - 1], j - 1, b_sum_till[N - 1], N - 1, best_i_bw[j - 1], j - 1, a[j] + b_sum_till[j - 1] + b_sum_till[N - 1] + best_i_bw[j + 1]);
 		#endif
 
 		// Case 3
