90c4eca547e9af402c0b2eb8cce29811f46e5232
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-02-17 20:20:33 +0530

    Different Strategy

--- Table-Sum-INOI1202/main.cpp
+++ Table-Sum-INOI1202/main.cpp
@@ -6,39 +6,22 @@ size_t N;
 
 int main () {
     cin >> N;
-    uint a[N];
+    multiset<uint> nums; uint a[N];
 
     for (size_t i = 0; i < N; i++) {
-        cin >> a[i];
-    }
-
-    uint max_ind = N - 1; // tracks INDEX of max. number
-
-    // find initial max. number
-    for (long int j = max_ind; j >= 0; j--) {
-        if (a[j] + ((j + 0) % N) + 1 > a[max_ind] + ((max_ind + 0) % N) + 1) max_ind = j;
+        cin >> a[i]; a[i] += i + 1;
+        nums.insert(a[i]);
     }
 
     for (size_t i = 0; i < N; i++) {
-        if (((max_ind + i) % N) + 1 == 1) { // max num. ONLY shifts when +N falls to +1
+        cout << *nums.rbegin() + i << " ";
 
-            #ifdef DEBUG
-            for (size_t k = 0; k < N; k++) cout << a[k] << "\t";
-            cout << endl;
-            for (size_t k = 0; k < N; k++) cout << ((k + i) % N) + 1 << "\t";
-            cout << endl;
-            #endif
+        // update Nth number which has fallen off
+        uint n = N - 1 - i;
 
-            for (long int j = max_ind; j >= 0; j--) {
-                if (a[j] + ((j + i) % N) + 1 > a[max_ind] + ((max_ind + i) % N) + 1) max_ind = j;
-            }
-
-            #ifdef DEBUG
-            for (size_t k = 0; k < max_ind; k++) cout << "\t";
-            cout << "|\n\n";
-            #endif
-        }
-
-        cout << a[max_ind] + ((max_ind + i) % N) + 1 << endl;
+        nums.erase (nums.find(a[n]));
+        a[n] -= n; // revert to a[n] + 1
+        nums.insert (a[n]);
     }
+    cout << endl;
 }
