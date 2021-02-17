164691eac7837b835b4b28d33345e77c352d09a0
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-02-17 13:20:20 +0530

    Attempt

--- Table-Sum-INOI1202/main.cpp
+++ Table-Sum-INOI1202/main.cpp
@@ -6,15 +6,39 @@ size_t N;
 
 int main () {
     cin >> N;
-    uint a [N];
+    uint a[N];
 
-    uint max_ele = 0;
     for (size_t i = 0; i < N; i++) {
         cin >> a[i];
     }
 
-    uint max_ele = 0;
+    uint max_ind = N - 1; // tracks INDEX of max. number
+
+    // find initial max. number
+    for (long int j = max_ind; j >= 0; j--) {
+        if (a[j] + ((j + 0) % N) + 1 > a[max_ind] + ((max_ind + 0) % N) + 1) max_ind = j;
+    }
+
     for (size_t i = 0; i < N; i++) {
-        
+        if (((max_ind + i) % N) + 1 == 1) { // max num. ONLY shifts when +N falls to +1
+
+            #ifdef DEBUG
+            for (size_t k = 0; k < N; k++) cout << a[k] << "\t";
+            cout << endl;
+            for (size_t k = 0; k < N; k++) cout << ((k + i) % N) + 1 << "\t";
+            cout << endl;
+            #endif
+
+            for (long int j = max_ind; j >= 0; j--) {
+                if (a[j] + ((j + i) % N) + 1 > a[max_ind] + ((max_ind + i) % N) + 1) max_ind = j;
+            }
+
+            #ifdef DEBUG
+            for (size_t k = 0; k < max_ind; k++) cout << "\t";
+            cout << "|\n\n";
+            #endif
+        }
+
+        cout << a[max_ind] + ((max_ind + i) % N) + 1 << endl;
     }
 }
