8162647576e49830dd2853c5b89902061081b6e2
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-02-20 21:31:44 +0530

    Reworded faster solution With much less memory rewriting

--- Department-Strengths-INOI2001/main.cpp
+++ Department-Strengths-INOI2001/main.cpp
@@ -2,75 +2,27 @@
 
 using namespace std;
 
-const size_t MAX_N = 10e5;
+const size_t MAX_N = 10e6;
 
 vector<uint> emps [MAX_N]; //emps[i] contains all children of i
 
-bool dept_head [MAX_N];
-bool visited [MAX_N];
+bool visited [MAX_N], visited2[MAX_N];
+uint min_e, max_e, num_e, strength;
 
-uint descendant_num (uint n) {
-	uint ans = 0;
+void compute_strength (uint n, uint lvl) {
+	visited2[n] = true;
+	strength += lvl;
 
-	for (uint child: emps[n]) {
-		if (visited[child]) continue;
-
-		visited[child] = true;
-		ans++; // account for child being descendant as well
-		ans += descendant_num(child);
-	}
-
-	return ans;
+	for (uint child: emps[n])
+		if (visited2[child] != true) compute_strength (child, lvl + 1);
 }
 
-// marks all descendants of node as false
-void mark_descendants (uint n) {
+void dfs (uint n) {
 	visited[n] = true;
+	num_e++; min_e = min (n, min_e); max_e = max (n, max_e);
 
-	for (uint child: emps[n]) {
-		if (visited[child] == true) continue;
-
-		dept_head[child] = false; visited[child] = true;
-		mark_descendants (child);
-	}
-}
-
-uint level_sum (uint n, int prev_lev) {
-	int ans = prev_lev + 1;
-	visited[n] = true;
-
-	for (uint child: emps[n]) {
-		if (visited[child] == true) continue;
-
-		visited[child] = true;
-		ans += level_sum (child, prev_lev + 1);
-	}
-
-	return ans;
-}
-
-uint max_id (uint n) {
-	uint ans = n;
-	for (uint child: emps[n]) {
-		if (visited[child] == true) continue;
-
-		visited[child] = true;
-		ans = max (max_id(child), ans);
-	}
-
-	return ans;
-}
-
-uint min_id (uint n) {
-	uint ans = n;
-	for (uint child: emps[n]) {
-		if (visited[child] == true) continue;
-
-		visited[child] = true;
-		ans = min (max_id(child), ans);
-	}
-
-	return ans;
+	for (uint child: emps[n])
+		if (visited[child] != true)	dfs(child);
 }
 
 int main () {
@@ -84,45 +36,32 @@ int main () {
 			uint u, v; cin >> u >> v; u--; v--; emps[u].push_back(v); emps[v].push_back(u);
 		}
 
-		// figure out the managers
-		fill_n (dept_head, N, true);
-		for (size_t n = 0; n < N; n++) mark_descendants (n);
-		fill_n (visited, N, false);
-		// now nodes which are managers are the only ones marked true
+		fill_n (visited, N, false); fill_n (visited2, N, false);
 
 		long int odd = 0, even = 0;
 		for (size_t n = 0; n < N; n++)
-			if (dept_head[n] == true) {
-				uint desc_num = descendant_num(n);
-				fill_n (visited, N, false);
-				
-				#ifdef DEBUG
-				printf ("%ld identified as part of a new dept.\nnumber of descendants: %d\n", n + 1, desc_num);
-				#endif
+			if (visited[n] != true) {
+				max_e = n; min_e = n; num_e = 0; strength = 0;
+				dfs(n);
 
-				if (desc_num % 2 == 0) {
-					uint head = min_id(n);
-					fill_n (visited, N, false);
-					uint res = level_sum (head, 0);
-
+				if (num_e % 2 == 0) {
+					compute_strength (min_e, 1);
+					
 					#ifdef DEBUG
-					printf ("Strength of dept. is %d with head as: %d\n", res, head + 1);
+					printf ("%d is head of dept. of %d people with strength %d\n", min_e + 1, num_e, strength);
 					#endif
-
-					even += res;
-				} else {
-					uint head = max_id(n);
-					fill_n (visited, N, false);
-					uint res = level_sum (head, 0);
-
-					#ifdef DEBUG
-					printf ("Strength of dept. is %d with head as: %d\n", res, head + 1);
-					#endif
-
-					odd += res;
+					
+					even += strength;
 				}
-
-				fill_n (visited, N, false);
+				else {
+					compute_strength (max_e, 1);
+										
+					#ifdef DEBUG
+					printf ("%d is head of dept. of %d people with strength %d\n", max_e + 1, num_e, strength);
+					#endif
+					
+					odd += strength;
+				}
 			}
 
 		printf ("%ld %ld\n", even, odd);
