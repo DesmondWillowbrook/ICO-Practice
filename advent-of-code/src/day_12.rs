c060f398b9fe8ab411937f7d776c58742f09b86e
Kartavya Vashishtha <sendtokartavya@gmail.com>
2021-12-12 18:05:17 +0530

    day 12 part 2

--- advent-of-code/src/day_12.rs
+++ advent-of-code/src/day_12.rs
@@ -28,7 +28,7 @@ impl<T> IndexMut<Node> for [T] {
 	}
 }
 
-fn trace (node: Node, out: &[Vec<Node>], small_caves_explored: &mut Vec<bool>) -> u64 {
+fn trace_1 (node: Node, out: &[Vec<Node>], small_caves_explored: &mut Vec<bool>) -> u64 {
 	let mut ans = 0;
 
 	println!("Current node is {}", usize::from(node));
@@ -38,11 +38,11 @@ fn trace (node: Node, out: &[Vec<Node>], small_caves_explored: &mut Vec<bool>) -
 		ans += match o {
 			Node::Small(_) => if !small_caves_explored[usize::from(*o)] {
 				small_caves_explored[usize::from(*o)] = true;
-				let ret = trace (*o, out, small_caves_explored);
+				let ret = trace_1 (*o, out, small_caves_explored);
 				small_caves_explored[usize::from(*o)] = false;
 				ret
 			} else {0},
-			Node::Large(_) => trace (*o, out, small_caves_explored),
+			Node::Large(_) => trace_1 (*o, out, small_caves_explored),
 			Node::End => 1,
 			Node::Start => 0,
 		};
@@ -85,5 +85,70 @@ pub fn part_1<T: std::io::BufRead> (lines: std::io::Lines<T>) -> u64 {
 
 	//vec![false; edges.len()] is upper bound on number of small nodes. we could count small nodes separately
 	//to preserve memory, but I don't want to riddle this already ugly code with more C++-like writing
-	trace(Node::Start, &edges, &mut vec![false; edges.len()])
+	trace_1(Node::Start, &edges, &mut vec![false; edges.len()])
+}
+
+fn trace_2 (node: Node, out: &[Vec<Node>], small_caves_explored: &mut Vec<u8>, twice_cave_used: bool) -> u64 {
+	let mut ans = 0;
+
+	println!("Current node is {}", usize::from(node));
+	println!("Outbound nodes: {:?}", &out[node]);
+
+	for o in &out[node] {
+		ans += match o {
+			Node::Small(_) => if small_caves_explored[usize::from(*o)] == 0 {
+				small_caves_explored[usize::from(*o)] = 1;
+				let ret = trace_2 (*o, out, small_caves_explored, twice_cave_used);
+				small_caves_explored[usize::from(*o)] = 0;
+				ret
+			} else if small_caves_explored[usize::from(*o)] == 1 && !twice_cave_used{
+				small_caves_explored[usize::from(*o)] = 2;
+				let ret = trace_2 (*o, out, small_caves_explored, true);
+				small_caves_explored[usize::from(*o)] = 1;
+				ret
+			} else {0},
+			Node::Large(_) => trace_2 (*o, out, small_caves_explored, twice_cave_used),
+			Node::End => 1,
+			Node::Start => 0,
+		};
+	};
+
+	ans
+}
+
+pub fn part_2<T: std::io::BufRead> (lines: std::io::Lines<T>) -> u64 {
+	let mut hs = HashMap::from([("start", Node::Start), ("end", Node::End)]);
+
+	let mut edges: Vec<Vec<Node>> = vec![vec![], vec![]];
+	let lines = lines.map(Result::unwrap).collect_vec();
+	
+	for l in lines.iter().map(|s| s.as_str()) {
+		let (e1, e2) = 
+			l.split('-')
+				.map(|s| {
+					if hs.contains_key(s) {return hs[s]}
+
+					hs.insert(s, if s.chars().next().unwrap().is_uppercase() {
+						 Node::Large(hs.len())
+					} else {
+						Node::Small(hs.len())
+					});
+					hs[&s]
+				})
+				.collect_tuple().unwrap();
+
+		if edges.len() <= max(e1.into(), e2.into()) {
+			let required_len = max(<usize>::from(e1),  <usize>::from(e2)) + 1;
+
+			edges.extend((0..(required_len - edges.len())).map(|_| Vec::new()))
+		}		
+		edges.as_mut_slice()[e1].push(e2);
+		edges.as_mut_slice()[e2].push(e1);
+	}
+
+	println!("{:?}", &edges);
+
+	//vec![false; edges.len()] is upper bound on number of small nodes. we could count small nodes separately
+	//to preserve memory, but I don't want to riddle this already ugly code with more C++-like writing
+	trace_2(Node::Start, &edges, &mut vec![0; edges.len()], false)
 }
