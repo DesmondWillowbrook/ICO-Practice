0ff37d39ef38b749ac55085cb6d15dd0b157f05a
Kartavya Vashishtha <sendtokartavya@gmail.com>
2021-12-24 19:05:33 +0530

    Hotel Queries Segtree solution Quite a fun problem!

--- cses.fi/Hotel-Queries-1143/main.cpp
+++ cses.fi/Hotel-Queries-1143/main.cpp
@@ -2,38 +2,110 @@
 
 using namespace std;
 
-const long int MAX_ROOM = 1e9;
+#ifdef DEBUG
+void print_segtree(int* segtree, uint power_of_two) {
+	for (uint i = 0, pow_two = 1; i < power_of_two; i++) {
+		printf ("%02d ", i);
+		if (i == pow_two - 1) {
+			printf ("| ");
+			pow_two *= 2;
+		}
+	}
+	cout << endl;
+	for (uint i = 0, pow_two = 1; i < power_of_two; i++) {
+		printf ("%02d ", segtree[i]);
+		if (i == pow_two - 1) {
+			printf ("| ");
+			pow_two *= 2;
+		}
+	}
+	cout << endl;
+}
+#endif
 
 int main () {
 	uint n, m; cin >> n >> m;
 
-	map<uint, priority_queue<uint, vector<uint>, greater<uint>>> frhn;
-	for (uint i = 1; i <= n; i++) {
+	// number of free rooms to minimum hotel number mapping
+	uint power_of_two = 1; while (power_of_two < n) {power_of_two *= 2;}
+	int segtree[2 * power_of_two]; segtree[0] = -1; // not used
+	for (uint i = power_of_two; i < power_of_two+n; i++) {
 		uint r_num; cin >> r_num;
-		frhn[r_num].push(i);
+		segtree[i] = r_num;
+	
+	}
+	// insert padding
+	for (uint i = power_of_two+n; i < 2 * power_of_two; i++) {
+		segtree[i] = -1;
 	}
 
+	//construct tree
+	for (int i = 2*power_of_two - 2; i >= 1; i -= 2) {
+		segtree[i/2] = max(segtree[i], segtree[i + 1]);
+	}
+
+	#ifdef DEBUG 
+	print_segtree(segtree, 2*power_of_two);
+	#endif
+
+
 	for (uint i = 1; i <= m; i++) {
-		uint room_req; cin >> room_req;
-		uint hotel = n + 1; auto frh_ptr = frhn.end();
-		for (auto it = frhn.lower_bound(room_req); it != frhn.end(); it++) {
-			auto q = (*it).second;
-			if (!q.empty() && q.top() < hotel) {
-				hotel = q.top();
-				frh_ptr = it;
-			}
-		}
-		if (hotel > n) {
-			printf ("0\n");
-			continue;
-		}
+		int room_req; cin >> room_req;
 		#ifdef DEBUG
-		printf("Available smallest hotel %d has %d rooms empty\n", hotel, (*frh_ptr).first);
+		printf ("Required rooms: %d\n", room_req);
 		#endif
 		
-		(*frh_ptr).second.pop(); // remove hotel from this queue, as it no longer has the same amount of room
-		frhn[(*frh_ptr).first - room_req].push(hotel);
-		printf ("%d\n", hotel);
+		// check if there is no hotel with enough rooms
+		if (segtree[1] < room_req) {
+			printf ("0");
+			#ifndef DEBUG
+			cout << " ";
+			#endif
+			#ifdef DEBUG
+			cout << endl;
+			#endif
+			continue;
+		}
+
+		uint idx = 1;
+		while (idx < power_of_two) {
+			#ifdef DEBUG
+			printf ("Current idx: %02d, max val: %d\n", idx, segtree[idx]);
+			#endif
+			if (segtree[2 * idx] < room_req) { //if right side smaller than required
+				idx = (idx*2) + 1; //then go to left side
+			} else if (segtree[2 * idx + 1] < room_req) { //if left side smaller than required
+				idx = idx*2; //then go to right side
+			} else { //if both sides have more than required
+				idx = idx*2; //then go to right side: it will have smaller hotel number
+			}
+		}
+
+		int hotel = idx - power_of_two + 1;
+		segtree[idx] -= room_req;
+
+		#ifdef DEBUG
+		printf ("Smallest hotel with req rooms: %d (index %d)\nUpdating segtree\n", hotel, idx);
+		#endif
+
+		//update tree
+		idx /= 2;
+		for (; idx >= 1; idx /= 2) {
+			segtree[idx] = max(segtree[2*idx], segtree[2*idx + 1]);
+		}
+
+		#ifdef DEBUG 
+		print_segtree(segtree, 2*power_of_two);
+		#endif
+
+		printf ("%d", hotel);
+
+		#ifndef DEBUG
+		cout << " ";
+		#endif
+		#ifdef DEBUG
+		cout << endl;
+		#endif
 	}
 	cout << endl;
 }
