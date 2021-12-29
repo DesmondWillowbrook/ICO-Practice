73fa96ec8344130a47c4a1aee56dab20a4911d25
Kartavya Vashishtha <sendtokartavya@gmail.com>
2021-12-29 14:32:59 +0530

    Two Paths bottom up soln Instead of adding an extra 0 column/row to compensate for min, don't include current element Both work, but I implemented this approach correctly first

--- codechef.com/INOIPRAC/2-Two-Paths-TWOPATHS/main.cpp
+++ codechef.com/INOIPRAC/2-Two-Paths-TWOPATHS/main.cpp
@@ -7,92 +7,90 @@ int n, m, k;
 int arr[N + 1][M + 1];
 long int dp_max[N + 1][M + 1][K + 1], dp_min[N + 1][M + 1][K + 1];
 
-long int big (int i, int j, int turn) {
-	if (dp_max[i][j][turn] != -INT_MAX) {return dp_max[i][j][turn];}
-	long int ans = -INT_MAX;
-	/*if (j == 0) { //single column, max = sum of that column
-		ans = arr[i][j];
-	} else*/ if (turn == 0 /*cannot turn further*/ || i == 0) { 
-		//sum = sum of this column + sum of previous column with same height
-		// turning means considering sum of previous column with one less height as well
-		ans = arr[i][j] + big(i, j - 1, turn);
-	} else {
-		ans = arr[i][j] + max (big(i - 1, j - 1, turn - 1), big(i - 1, j, turn));
-	}
-	#ifdef DEBUG
-	printf ("Maximum sum achievable at (%d, %d, %d) is %ld\n", i, j, turn, ans);
-	#endif
-
-	dp_max[i][j][turn] = ans;
-	return ans;
-}
-
-long int small (int i, int j, int turn) {
-	if (dp_min[i][j][turn] != -INT_MAX) {return dp_min[i][j][turn];}
-	long int ans = -INT_MAX;
-	/*if (j == 0) { //single column, max = sum of that column
-		ans = arr[i][j];
-	} else*/ if (turn == 0 /*cannot turn further*/ || i == 0) { 
-		//sum = sum of this column + sum of previous column with same height
-		// turning means considering sum of previous column with one less height as well
-		ans = arr[i][j] + small(i, j - 1, turn);
-	} else {
-		ans = arr[i][j] + min (small(i - 1, j - 1, turn - 1), small(i - 1, j, turn));
-	}
-	#ifdef DEBUG
-	printf ("Maximum sum achievable at (%d, %d, %d) is %ld\n", i, j, turn, ans);
-	#endif
-
-	dp_min[i][j][turn] = ans;
-	return ans;
-}
-
 int main () {
 	int t; cin >> t;
 	while (t--) {
 		cin >> n >> m >> k;
 
 		for (int i = 0; i < n; i++) {
-			for (int j = 1; j <= m; j++) {
-				for (int turn = 0; turn <= k; turn++) {
-					dp_max[i][j][turn] = -INT_MAX;
-					dp_min[i][j][turn] = -INT_MAX;
-				}
-			}
-		}
-		for (int i = 0; i < n; i++) {
-			arr[i][0] = 0;
-			for (int turn = 0; turn <= k; turn++) {
-				dp_max[i][0][turn] = 0;
-				dp_min[i][0][turn] = 0;
-			}
-		}
-		/*
-		  j -->
-		i 0 X X X
-		| 0 X X X
-		v 0 X X X
-		*/
-		
-		for (int i = 0; i < n; i++) {
-			for (int j = 1; j <= m; j++) {
+			for (int j = 0; j < m; j++) {
 				cin >> arr[i][j];
 			}
 		}
 
 		for (int i = 0; i < n; i++) {
-			for (int j = 1; j <= m; j++) {
-				arr[i][j] += arr[i - 1][j];
+			for (int j = 1; j < m; j++) {
+				arr[i][j] += arr[i][j - 1];
+			}
+		}
+
+		// calc min arr
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				for (int turn = 0; turn <= k; turn++) {
+					// element at current position not included
+					// because both of our "paths" are inclusive
+					// => when subtracting, we want to cut out the 
+					// portion which is min for that position,
+					// EXCLUDING that position
+					if (j > 0) {dp_min[i][j][turn] = arr[i][j - 1];}
+					else {dp_min[i][j][turn] = 0;}
+
+					//there are no rows before this one
+					//thus min sum is sum of this row only
+					if (i == 0) {
+						continue;
+					}
+					//if we can't turn or we're at the first row
+					//only option is to go back one row
+					if (j == 0 || turn == 0) {
+						dp_min[i][j][turn] += dp_min[i - 1][j][turn];
+						continue;
+					}
+					//general case:
+					//min = sum of this row NOT including current element (already added)
+					// + either max of previous row with same column height
+					// or max of previous row with height of column reduced by 1
+					dp_min[i][j][turn] += 
+						min(dp_min[i - 1][j][turn], dp_min[i - 1][j - 1][turn - 1]);
+				}
+			}
+		}
+
+		// calc max arr
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				for (int turn = 0; turn <= k; turn++) {
+					// element at current position included
+					dp_max[i][j][turn] = arr[i][j];		
+					//there are no rows before this one
+					//thus max sum is sum of this row only
+					if (i == 0) {
+						continue;
+					}
+					//if we can't turn or we're at the first row
+					//only option is to go back one row
+					if (j == 0 || turn == 0) {
+						dp_max[i][j][turn] += dp_max[i - 1][j][turn];
+						continue;
+					}
+					//general case:
+					//max = sum of this row including current element (already added)
+					// + either max of previous row with same column height
+					// or max of previous row with height of column reduced by 1
+					dp_max[i][j][turn] += 
+						max(dp_max[i - 1][j][turn], dp_max[i - 1][j - 1][turn - 1]);
+				}
 			}
 		}
 
 		long int ans = -INT_MAX;
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < i - k; j++) {
-				ans = max(ans, big(n - 1, i, k) - small (n - 1, j, k));
+		for (int j = 0; j < m; j++) {
+			for (int i = j + k + 1; i < m; i++) {
+				ans = max(ans, dp_max[n - 1][i][k] - dp_min[n - 1][j][k]);
 				#ifdef DEBUG
 				printf ("Big for (%d, %d, %d) = %ld, small for (%d, %d, %d) = %ld\n",
-					n - 1, i, k, big(n - 1, i, k), n - 1, j, k, small (n - 1, j, k));
+					n - 1, i, k, dp_max[n - 1][i][k], n - 1, j, k, dp_min[n - 1][j][k]);
 				#endif
 			}
 		}
