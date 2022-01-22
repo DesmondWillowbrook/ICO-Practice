54d3e6ebdfe149b316f992fd15be616c130ee39f
Kartavya Vashishtha <sendtokartavya@gmail.com>
2022-01-22 15:16:44 +0530

    remove debug statements

--- cses.fi/List-Removals-1749/main.cpp
+++ cses.fi/List-Removals-1749/main.cpp
@@ -7,10 +7,6 @@ int* segtree;
 int* arr;
 
 void build (int l, int r, int pos) {
-	#ifdef DEBUG
-	printf ("build (%d, %d, %d)\n", l, r, pos);
-	#endif
-
 	if (l == r) {
 		cin >> arr[l];
 		segtree[pos] = 1;
@@ -23,10 +19,6 @@ void build (int l, int r, int pos) {
 }
 
 int upd_ind (int l, int r, int idx, int pos) {
-	#ifdef DEBUG
-	printf ("upd_ind (%d, %d, %d, %d)\n", l, r, idx, pos);
-	#endif
-
 	if (l == r) {
 		segtree[pos] = 0;
 		return l;
@@ -43,40 +35,16 @@ int upd_ind (int l, int r, int idx, int pos) {
 	return ans;
 }
 
-#ifdef DEBUG
-void preview_segtree () {
-	cout << endl;
-	for (int i = 1; i < 4*n; i++) {
-		printf ("%02d ", i);
-	} cout << endl;
-	for (int i = 1; i < 4*n; i++) {
-		printf ("%02d ", segtree[i]);
-	} cout << endl;
-}
-#endif
-
 int main () {
 	cin >> n;
 	segtree = (int*) alloca (4 * n * sizeof(n));
 	arr = (int*) alloca (n * sizeof(n) + 1);
 
-	#ifdef DEBUG
-	fill_n(segtree, 4*n, 0);
-	#endif
 	build(1, n, 1);
-	#ifdef DEBUG
-	preview_segtree ();
-	#endif
 
 	for (int i = 0; i < n; i++) {
 		int idx; cin >> idx;
-		#ifdef DEBUG
-		cout << "Deleting number at index " << idx << "\n";
-		#endif
 		cout << arr[upd_ind(1, n, idx, 1)] << " ";
-		#ifdef DEBUG
-		preview_segtree ();
-		#endif
 	}
 	cout << endl;
 }
